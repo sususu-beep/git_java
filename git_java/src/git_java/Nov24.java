16b33826ec9270e3f208dd59bfb863108d16d157
yoo <yoosu58@gmail.com>
2025-11-24 14:11:08 +0900

    브랜치테스트에서작업

--- git_java/src/git_java/Nov24.java
+++ git_java/src/git_java/Nov24.java
@@ -4,7 +4,7 @@ public class Nov24 {
 	
 	public static void main (String[] args) {
 		System.out.println("초기설정");
-		System.out.println("branch_test 에서 작업");
+		System.out.println("branch_test에서 작업");
 	}
 
 }
